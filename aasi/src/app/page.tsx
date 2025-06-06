590b9479bbabc523e52025bd41f5e80bbb27e9ee
Genisson Emilio <esgenisson@gmail.com>
2025-06-06 20:49:08 -0300

    go

---
+++ aasi/src/app/page.tsx
@@ -0,0 +1,9 @@
+const Home: React.FC = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <h1 className="text-4xl font-bold">Em Breve</h1>
+        </div>
+    )
+}
+
+export default Home;
