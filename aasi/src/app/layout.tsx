4d7abd7418f27e823031ef3071d6b9bf16757922
Genisson Emilio <esgenisson@gmail.com>
2025-06-06 21:32:17 -0300

    precaucao

--- aasi/src/app/layout.tsx
+++ aasi/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { fonte } from "../assets/fonts/fonte.TTF";
 import "./globals.css";
 
 const geistSans = Geist({
