23f024666fda1256ff700d05cf4a4e118a4b3266
Genisson Emilio <esgenisson@gmail.com>
2025-06-06 22:48:49 -0300

    Update NightSkyComponent.tsx

--- aasi/src/components/NightSkyComponent.tsx
+++ aasi/src/components/NightSkyComponent.tsx
@@ -21,7 +21,7 @@ const NightSkyComponent = () => {
     <div style={{ width: "100%", height: "100vh" }}>
       {React.createElement('night-sky', {
         id: 'nightSky',
-        layers: 1,
+        layers: 4,
         density: 30,
         'velocity-x': 60,
         'velocity-y': 60,
