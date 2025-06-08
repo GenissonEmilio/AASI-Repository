e7394e614f26871734b9cb2f98042398fa39e477
invencaosts <gui.inven@gmail.com>
2025-06-08 18:33:01 -0300

    testando projeto

--- aasi/src/app/page.tsx
+++ aasi/src/app/page.tsx
@@ -1,27 +1,34 @@
 import NightSkyComponent from "@/components/NightSkyComponent";
-import GlitchImage from "@/components/GlitchImage";
+import CardInfo from "@/components/CardInfo";
 
 export default function Home() {
   return (
     <div className="relative w-full min-h-screen flex items-center justify-center overflow-hidden">
-      <div className="relative flex flex-col items-center justify-center w-full h-full min-h-screen overflow-hidden px-4">
+      <div className="relative flex items-center justify-center w-full h-full min-h-screen overflow-hidden">
         <div className="absolute inset-0 -z-10">
           <NightSkyComponent />
         </div>
 
-        <h1
-          style={{ fontFamily: "fonte" }}
-          className="text-3xl bg-gradient-to-r from-[#6B2BDA] to-fuchsia-600 text-transparent bg-clip-text text-center"
-        >
-          Em breve
-        </h1>
-
-        <div className="absolute bottom-0 right-0 z-10 translate-y-1/4 translate-x-1/4">
-          <GlitchImage
-            src="/images/samurai.png"
-            alt="Descrição da imagem"
-            className="w-[180px] h-[300px] object-cover opacity-30 blur-xl"
-          />
+        {/* Conteúdo centralizado */}
+        <div className="relative bottom-10 flex flex-col items-center justify-center px-4">
+          <h1
+            style={{ fontFamily: "fonte" }}
+            className="text-3xl bg-gradient-to-r from-[#6B2BDA] to-fuchsia-600 text-transparent bg-clip-text text-center"
+          >
+            Aguarde...
+          </h1>
+          <p
+            style={{ fontFamily: "Nasalization" }}
+            className="text-sm text-center mb-8"
+          >
+            O primeiro card está na embalagem. <br /> O destino começa aqui...
+          </p>
+          <CardInfo />
+          <div className="mt-4">
+            <p className="text-md bg-gradient-to-r from-[#6B2BDA] to-fuchsia-600 text-transparent bg-clip-text text-center">
+              Colecione. Descubra. Conquiste.
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- aasi/src/components/CardInfo.tsx
+++ aasi/src/components/CardInfo.tsx
@@ -1,13 +1,39 @@
-const CardInfo = () => {
+import GlitchImage from "@/components/GlitchImage";
+
+export default function CardInfo() {
   return (
-    <div className="w-[200px] h-[300px] bg-[#6B2BDA] rounded border-4 border-blue-600">
-      <p>
-        Um guerreiro surgiu das sombras do futuro...
-        Colecione. Descubra. Conquiste.
-        O primeiro card está na embalagem. O destino começa aqui.
-      </p>
+    <div
+      style={{ fontFamily: "Nasalization" }}
+      className="text-start flex items-center justify-center flex-col gap-4 p-4 w-[300px] h-[410px]
+      bg-gradient-to-r from-[#6B2BDA] to-fuchsia-600 rounded border-6 border-fuchsia-200"
+    >
+      <div
+        className="w-full
+      bg-gradient-to-r from-[#6B2BDA] to-fuchsia-600 p-2 shadow-lg/40 rounded"
+      >
+        <p>Guerreiro do Futuro</p>
+      </div>
+
+      <div
+        className="flex items-center justify-center w-[90%] 
+      bg-gradient-to-r from-[#6B2BDA] to-fuchsia-600 p-2 shadow-lg/40 rounded"
+      >
+        <GlitchImage
+          src="/images/samurai.png"
+          alt="Descrição da imagem"
+          className="w-[120px] h-[180px] object-cover opacity-0 blur-xl"
+        />
+      </div>
+
+      <div
+        className="w-full
+      bg-gradient-to-r from-[#6B2BDA] to-fuchsia-600 p-2 shadow-lg/40 rounded"
+      >
+        <p className="text-sm">
+          Este guerreiro surgiu da sombra de um futuro distante, onde a
+          tecnologia e a tradição se entrelaçam.
+        </p>
+      </div>
     </div>
   );
 }
-
-export default CardInfo;
