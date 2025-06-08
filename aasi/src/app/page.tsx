9d25c0554f57ce696e141b2cd0978e36d6bc5487
invencaosts <gui.inven@gmail.com>
2025-06-08 17:02:04 -0300

    atualizando home

--- aasi/src/app/page.tsx
+++ aasi/src/app/page.tsx
@@ -1,25 +1,29 @@
 import NightSkyComponent from "@/components/NightSkyComponent";
-// import Image from "next/image";
 import GlitchImage from "@/components/GlitchImage";
 
 export default function Home() {
   return (
-    <div className="w-full flex items-center justify-center overflow-hidden">
-       <NightSkyComponent />
-      <div className="absolute text-3xl text-[#6B2BDA] text-center">
-        <h1 style={{ fontFamily: 'fonte' }} className="bg-gradient-to-r from-[#6B2BDA] to-fuchsia-600 text-transparent bg-clip-text">Em breve</h1>
-        <div>
-          <p style={{ fontFamily: 'nasalization' }} className="text-sm text-white">Obs. Guardem os cards para uma surpresa.</p>
+    <div className="relative w-full min-h-screen flex items-center justify-center overflow-hidden">
+      <div className="relative flex flex-col items-center justify-center w-full h-full min-h-screen overflow-hidden px-4">
+        <div className="absolute inset-0 -z-10">
+          <NightSkyComponent />
+        </div>
+
+        <h1
+          style={{ fontFamily: "fonte" }}
+          className="text-3xl bg-gradient-to-r from-[#6B2BDA] to-fuchsia-600 text-transparent bg-clip-text text-center"
+        >
+          Em breve
+        </h1>
+
+        <div className="absolute bottom-0 right-0 z-10 translate-y-1/4 translate-x-1/4">
+          <GlitchImage
+            src="/images/samurai.png"
+            alt="Descrição da imagem"
+            className="w-[180px] h-[300px] object-cover opacity-30 blur-xl"
+          />
         </div>
       </div>
-
-      {/* <Image src={'/images/samurai.png'} alt="samurai" width={150} height={250} className="absolute inset-y-130 bottom-0 right-0" /> */}
-      <GlitchImage 
-        src="/images/samurai.png" 
-        alt="Descrição da imagem"
-        className="w-[150px] h-[250px] absolute inset-y-130 bottom-0 right-0 object-cover opacity-30 blur-xl"
-      />
-      
     </div>
   );
 }
