771147c692fc2bdd187414216538846919abe5e8
Guilherme da Invenção Santos <gui.inven@gmail.com>
2025-06-14 17:57:45 -0300

    redirecionando trafego

---
+++ aasi/src/app/CardPage/page.tsx
@@ -0,0 +1,47 @@
+import NightSkyComponent from "@/components/NightSkyComponent";
+import Trail from "@/components/Trail";
+import Image from "next/image";
+
+export default function CardPage() {
+  return (
+    <div className="relative w-full min-h-screen flex items-center justify-center overflow-hidden">
+      <Trail />
+
+      {/* Componente do céu noturno */}
+      <div className="relative flex items-center justify-center w-full h-full min-h-screen overflow-hidden">
+        <div className="absolute inset-0 -z-10">
+          <NightSkyComponent />
+        </div>
+
+        {/* Conteúdo centralizado */}
+        <div className="relative bottom-10 flex flex-col items-center justify-center px-4">
+          <h1 className="text-3xl bg-gradient-to-r from-[#6B2BDA] to-fuchsia-600 text-transparent bg-clip-text text-center">
+            Aguarde...
+          </h1>
+          <p
+            style={{ fontFamily: "Nasalization" }}
+            className="text-sm text-center mb-2"
+          >
+            O primeiro card está na embalagem. <br /> O destino começa aqui...
+          </p>
+          {/* <CardInfo /> */}
+          <Image
+            src="/images/card.png"
+            alt="Mascote do jogo"
+            width={300}
+            height={450}
+            className="object-cover"
+          />
+          <div className="absolute bottom-[57px] text-sm mt-2 text-center tracking-wide">
+            Guerreiro do Futuro
+          </div>
+          <div className="mt-2">
+            <p className="text-md bg-gradient-to-r from-[#6B2BDA] to-fuchsia-600 text-transparent bg-clip-text text-center">
+              Colecione. Descubra. Conquiste.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- aasi/src/app/page.tsx
+++ aasi/src/app/page.tsx
@@ -1,47 +1,17 @@
-import NightSkyComponent from "@/components/NightSkyComponent";
-import Trail from "@/components/Trail";
-import Image from "next/image";
+"use client";
+
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    window.location.href = "https://www.instagram.com/atleticahashiras/";
+  }, []);
+
   return (
-    <div className="relative w-full min-h-screen flex items-center justify-center overflow-hidden">
-      <Trail />
-
-      {/* Componente do céu noturno */}
-      <div className="relative flex items-center justify-center w-full h-full min-h-screen overflow-hidden">
-        <div className="absolute inset-0 -z-10">
-          <NightSkyComponent />
-        </div>
-
-        {/* Conteúdo centralizado */}
-        <div className="relative bottom-10 flex flex-col items-center justify-center px-4">
-          <h1 className="text-3xl bg-gradient-to-r from-[#6B2BDA] to-fuchsia-600 text-transparent bg-clip-text text-center">
-            Aguarde...
-          </h1>
-          <p
-            style={{ fontFamily: "Nasalization" }}
-            className="text-sm text-center mb-2"
-          >
-            O primeiro card está na embalagem. <br /> O destino começa aqui...
-          </p>
-          {/* <CardInfo /> */}
-          <Image
-            src="/images/card.png"
-            alt="Mascote do jogo"
-            width={300}
-            height={450}
-            className="object-cover"
-          />
-          <div className="absolute bottom-[57px] text-sm mt-2 text-center tracking-wide">
-            Guerreiro do Futuro
-          </div>
-          <div className="mt-2">
-            <p className="text-md bg-gradient-to-r from-[#6B2BDA] to-fuchsia-600 text-transparent bg-clip-text text-center">
-              Colecione. Descubra. Conquiste.
-            </p>
-          </div>
-        </div>
-      </div>
+    <div className="flex items-center justify-center h-screen">
+      <span className="text-xl md:text-4xl font-bold">
+        Redirecionando para o Instagram...
+      </span>
     </div>
   );
 }
